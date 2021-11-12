cff97cb809b491a3c6dc3dd114a12579e1b5a082
AnnieBox <todearannie@gmail.com>
2021-11-11 20:40:12 -0800

    Happy to conquer a big milestone!

--- public/a5/build/WhoToFollowList/who.js
+++ public/a5/build/WhoToFollowList/who.js
@@ -5,7 +5,7 @@ export default [
         userName: 'Relativity Space', handle: 'relativityspace', },
     {   avatarIcon: '../images/avatar_virgin-logo.png',
         userName: 'Virgin Galactic', handle: 'virgingalactic', },
-    {   avatarIcon: '../images/avatar_NASA2.png',
+    {   avatarIcon: '../images/avatar_NASA.png',
         userName: 'NASA', handle: 'NASA', },
     {   avatarIcon: '../images/avatar_tesla.png',
         userName: 'Tesla', handle: 'Tesla', },
