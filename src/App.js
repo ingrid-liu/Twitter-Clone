92320899504e3c458f4aba955dc6f42da9545a41
AnnieBox <todearannie@gmail.com>
2021-11-20 17:54:48 -0800

    setting browserouter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
+//import Library
 import React from "react";
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
-
+//import Library in local vendors
 import './vendors/bootstrap/css/bootstrap.min.css';
 import './vendors/bootstrap/bootstrap.min.css';
 import './vendors/fontawesome-pro/css/all.css';
+//import js component
+import HelloWorld from "./components/a6/HelloWorld";
+import Practice from "./components/a6/Practice";
+import Build from "./components/a6/Build";
 
-// import HelloWorld from "./components/a6/HelloWorld";
-// import Practice from "./components/a6/Practice";
-// import Build from "./components/a6/Build";
-
-import A6 from "./components/a6";
+import A6 from "./components/a6/HelloWorld";
+import A7 from "./components/a7/HelloWorld";
+import A9 from "./components/a8/HelloWorld";
 
 // import logo from './logo.svg';
 // import './App.css';
 
-
+// import {Provider} from "react-redux";
 
 
 function App() {
   return (
-      <BrowserRouter>
-          <Link to="/a6/practice">A6</Link> |
-      </BrowserRouter>
+      <div className="container">
+        <BrowserRouter>
+              {/*<Route path="/a6">*/}
+              {/*    <A6/>*/}
+              {/*</Route>*/}
+
+              {/*<Route path="/a9">*/}
+              {/*    <A9/>*/}
+              {/*</Route>*/}
+
+              {/*<Route path="/a7">*/}
+              {/*    <A7/>*/}
+              {/*</Route>*/}
+
+              <Link to="/a6">A6</Link> |
+              {/*<Link to="/a7/practice">A7</Link> |*/}
+              {/*<Link to="/a8/practice">A8</Link>*/}
+         </BrowserRouter>
+       </div>
+
   );
 }
 
@@ -61,4 +81,35 @@ export default App;
 //         <Route path="/a6/build" element={<Build />} />
 //
 //     </Routes>
+// </BrowserRouter>
+
+
+// latest tying work but not functional version
+// <BrowserRouter>
+//     <div className="container">
+//
+//         <Link to="/a6/practice">A6</Link> |
+//         <Link to="/a7/practice">A7</Link> |
+//         <Link to="/a8/practice">A8</Link>
+//
+//         {/*<Route path="/a9">*/}
+//         {/*    <A9/>*/}
+//         {/*</Route>*/}
+//
+//         {/*<Route path="/a6">*/}
+//         {/*    <A6/>*/}
+//         {/*</Route>*/}
+//
+//         {/*<Route path="/a7">*/}
+//         {/*    <A7/>*/}
+//         {/*</Route>*/}
+//
+//
+//         {/*<Route path={["/", "/a7", "/a7/practice"]} exact={true}>*/}
+//         {/*    <Practice/>*/}
+//         {/*</Route>*/}
+//         {/*<Route path="/a7/twitter">*/}
+//         {/*    <Build/>*/}
+//         {/*</Route>*/}
+//     </div>
 // </BrowserRouter>
--- src/components/a6/Practice/index.js
+++ src/components/a6/Practice/index.js
@@ -11,14 +11,30 @@ const Practice = () => {
 };
 export default Practice;
 
-// from previous practice.js
-// <>
-//     <h1>Practice</h1>
-//     <Link to="/a6/hello">
-//         Hello
-//     </Link> |
-//     <Link to="/a6/build">
-//         Build
-//     </Link>
+// import React from "react";
+// import {Link} from "react-router-dom";
+// import Classes from "./Classes";
+// import ConditionalOutput from "./ConditionOutput";
 //
-// </>
+// import TodoList from "./Todo/TodoList";
+//
+//
+// const Practice = () => {
+//
+//     return(
+//         <>
+//             <TodoList/>
+//             <Classes/>
+//             <h1>Practice</h1>
+//             <ConditionalOutput/>
+//             <Link to="/a6/hello">
+//                 Hello
+//             </Link>
+//             <Link to="/a6/twitter/home">
+//                 Build
+//             </Link>
+//         </>
+//     )
+// };
+//
+// export default Practice;
