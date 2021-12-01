5c120e698fcd1b15ff8da386575d4ec6932e2d7e
AnnieBox <todearannie@gmail.com>
2021-11-30 21:15:10 -0800

    remove mark note

--- src/components/a7/Build/HomeScreen/index.js
+++ src/components/a7/Build/HomeScreen/index.js
@@ -8,7 +8,6 @@ import WhatsHappening from "../WhatsHappening";
 const A7HomeScreen = () => {
     return(
         <div className="container">
-            <p>HomePage from A7</p>
             <div className="row mt-2">
                 <div className="col-2 ">
                     <NavigationSidebar active="home"/>
