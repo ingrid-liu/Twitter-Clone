ba80163402710d44d5367069ed6193171888e56a
AnnieBox <todearannie@gmail.com>
2021-12-10 13:01:26 -0800

    update profile editor

--- src/components/a8/Build/ProfileScreen/index.js
+++ src/components/a8/Build/ProfileScreen/index.js
@@ -7,42 +7,43 @@ import TweetList from "../TweetList";
 import PostSummaryList from "../PostSummaryList";
 import A7ProfileEdit from "../ProfileEdit";
 import Test from "../ReducerTester";
+import ExploreComponent from "../ExploreScreen/ExploreComponent";
 
 
 const A7ProfileScreen = () => {
     const [edit, setEdit] = useState(false);
     return (
-        <div className="row mt-2">
-            {/*IF ONLY DISPLAY THE PROFILE, USE THIS VERSION:*/}
+        // <div className="row mt-2">
+        //     {/*IF ONLY DISPLAY THE PROFILE, USE THIS VERSION:*/}
+        //
+        //     <div className="col-2">
+        //         <NavigationSidebar active={"profile"}/>
+        //     </div>
+        //     <div className="col-10 col-lg-6">
+        //         {/*when setEdit is false: profile page; when edit is true: edit page*/}
+        //         {!edit && <Profile setEdit={setEdit}/>}
+        //         {edit && <A7ProfileEdit setEdit={setEdit}/>}
+        //         {/*<Profile/>*/}
+        //         {/*{<Test/>}*/}
+        //
+        //     </div>
 
-            <div className="col-2">
-                <NavigationSidebar active={"profile"}/>
+            <div className="row mt-2">
+                <div className="col-2 ">
+                    <NavigationSidebar active={"profile"}/>
+                </div>
+                <div className="col-10 col-lg-6">
+                    {/*<Profile/>*/}
+                    {!edit && <Profile setEdit={setEdit}/>}
+                    {edit && <A7ProfileEdit setEdit={setEdit}/>}
+                    <TweetList/>
+                </div>
+                <div className="col-4 d-none d-lg-block">
+                    <PostSummaryList/>
+                </div>
             </div>
-            <div className="col-10 col-lg-6">
-                {/*when setEdit is false: profile page; when edit is true: edit page*/}
-                {!edit && <Profile setEdit={setEdit}/>}
-                {edit && <A7ProfileEdit setEdit={setEdit}/>}
-                {/*<Profile/>*/}
-                {/*{<Test/>}*/}
-
-            </div>
-
-            {/*<div className="row mt-2">*/}
-            {/*    <div className="col-2 ">*/}
-            {/*        <NavigationSidebar active={"profile"}/>*/}
-            {/*    </div>*/}
-            {/*    <div className="col-10 col-lg-6">*/}
-            {/*        <Profile/>*/}
-            {/*        /!*<TweetList/>*!/*/}
-            {/*    </div>*/}
-            {/*    <div className="col-4 d-none d-lg-block">*/}
-            {/*        <PostSummaryList/>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
 
 
-
-        </div>
     );
 }
 
