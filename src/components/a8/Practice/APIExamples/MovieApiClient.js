84e939c955164d95f5b97ae21cbb1f027e6134b1
AnnieBox <todearannie@gmail.com>
2021-12-09 21:51:22 -0800

    debug movieApiClient

--- src/components/a8/Practice/APIExamples/MovieApiClient.js
+++ src/components/a8/Practice/APIExamples/MovieApiClient.js
@@ -1,112 +1,103 @@
 import React, {useEffect, useState} from "react";
 const MovieApiClient = () => {
-    // declare empty array local state variable movies
     const [movies, setMovies] = useState([]);
 
-    // handle movie title input field change
-    const [movie, setMovie] = useState({title: '', rating: 2.5});
+    const [movie, setMovie] = useState({title: "", rating: 2.5});
 
-
-    // when the component first loads
+    // will render the component once
+    // Essentially get all
     useEffect(() =>
-            // send an HTTP request to this URL
             fetch('http://localhost:4000/api/movies')
                 .then(response => response.json())
                 .then(movies => setMovies(movies))
         , []);
-        // ↑ don't force re-render because state changed
 
-
-    // delete from front-end;
-    // Now is getting the updated data from the server
-    const deleteMovie = (movie) =>
-        fetch(`http://localhost:4000/api/movies/${movie._id}`, {
-            method: 'DELETE'
-        })
+    // delete in front end, currently we are getting the full udpated data
+    // from the server
+    const deleteMovie = (movie) => {
+        fetch(`http://localhost:4000/api/movies/${movie._id}`,
+            {method: "DELETE"})
             .then(response => response.json())
             .then(movies => setMovies(movies));
+    }
 
-
-    // send HTTP message to server URL /api/movies use HTTP POST
-    const onMovieTitleChange = (event) =>
+    const onMovieTitleChange = (event) => {
         setMovie({...movie, title: event.target.value});
+    }
 
-    const createMovieClickHandler = () =>
-        fetch('http://localhost:4000/api/movies', {
-            method: 'POST',
-            body: JSON.stringify(movie),
-            headers: {
+    const createMovieClickHandler = () => {
+        fetch("http://localhost:4000/api/movies", {
+            method: "POST",
+            body: JSON.stringify({...movie, _id: new Date().getTime().toString()}),
+            headers:{
                 'content-type': 'application/json'
             }
         })
             .then(response => response.json())
             .then(movies => setMovies(movies));
+    }
 
-    // send an HTTP PUT message to the saveMovie HTTP Webservice endpoint
-    const saveMovie = () =>
+
+    const saveMovie = () => {
         fetch(`http://localhost:4000/api/movies/${movie._id}`, {
-            method: 'PUT',
+            method: "PUT",
             body: JSON.stringify(movie),
-            headers: {
+            headers:{
                 'content-type': 'application/json'
             }
         })
             .then(response => response.json())
             .then(movies => setMovies(movies));
-
-
-
-
+    }
     return(
         <div>
             <h2>Movies</h2>
+            <input
+                className="form-control"
+                value={movie.title}
+                onChange = {onMovieTitleChange}
+                style={{width: "70%"}}
+            />
+            <button
+                onClick={createMovieClickHandler}
+                className = "btn btn-success"
+            >
+                Create
+            </button>
+
+            <button
+                onClick={saveMovie}
+                className = "btn btn-primary float-end ms-2"
+            >
+                Save
+            </button>
+
             <ul className="list-group">
                 {
                     movies.map((movie) =>
                         <li className="list-group-item"
                             key={movie._id}>
                             {movie.title} {movie.rating}
-
-
-                            <input className="form-control"
-                                   value={movie.title}
-                                   onChange={onMovieTitleChange}
-                                   style={{width: "70%"}}/>
-
-                            {/*Create Button*/}
                             <button
-                                onClick={createMovieClickHandler}
-                                className="btn btn-success float-end ms-2">
-                                Create
-                            </button>
-
-                            {/*Delete Button*/}
-                            <button
-                                onClick={()=>{deleteMovie(movie)}}
-                                className="btn btn-danger float-end  ms-2">
-                                Delete
-                            </button>
-
-                            {/*Edit Button*/}
-                            <button
-                                onClick={() => setMovie(movie)}
-                                className="btn btn-primary float-end ms-2">
+                                onClick = {() => setMovie(movie)}
+                                className = "btn btn-primary float-end ms-2"
+                            >
                                 Edit
                             </button>
 
-                            {/*Save Button*/}
+
                             <button
-                                onClick={saveMovie}
-                                className="btn btn-primary ms-2 float-end">
-                                Save
+                                onClick={()=>{deleteMovie(movie)}}
+                                className="btn btn-danger float-end"
+                            >
+                                Delete
                             </button>
-
-
 
 
                         </li>
                     )
                 }
+
             </ul>
         </div>
     )
